4a72505b0d4169fed17f5526200710daa6b1a9da
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-30 06:55:31 -0300

    Usando provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -16,26 +18,29 @@ class MyApp extends StatelessWidget {
       fontFamily: 'Lato',
     );
 
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: themeData.copyWith(
-        colorScheme: themeData.colorScheme.copyWith(
-          primary: Colors.purple,
-          secondary: Colors.deepOrange,
-        ),
-        textTheme: themeData.textTheme.copyWith(
-          titleLarge: const TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: themeData.copyWith(
+          colorScheme: themeData.colorScheme.copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          textTheme: themeData.textTheme.copyWith(
+            titleLarge: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontFamily: 'Lato',
+            ),
           ),
         ),
+        home: const ProductOverviewPage(),
+        routes: {
+          AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+        },
       ),
-      home: ProductOverviewPage(),
-      routes: {
-        AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
-      },
     );
   }
 }
