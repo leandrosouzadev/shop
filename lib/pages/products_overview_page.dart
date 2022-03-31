a8fa7be31f7f6ebfefaeb4e2cf8414892531176d
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-03-31 05:55:44 -0300

    Filtrar favoritos #01

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/components/product_grid.dart';
+import 'package:shop/models/product_list.dart';
+
+enum FilterOptions {
+  favorite,
+  all,
+}
 
 class ProductOverviewPage extends StatelessWidget {
   const ProductOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Minha Loja'),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.favorite,
+              ),
+              const PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.all,
+              ),
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          ),
+        ],
       ),
       body: const ProductGrid(),
     );
