4ac1800017ec1425872fdaa4b76454015f23a59b
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-02 10:01:46 -0300

    Navegação com Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -49,10 +50,12 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        home: const ProductOverviewPage(),
+        //home: const ProductOverviewPage(),
         routes: {
+          AppRoutes.home: (ctx) => const ProductOverviewPage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
+          AppRoutes.orders: (ctx) => const OrdersPage(),
         },
       ),
     );
