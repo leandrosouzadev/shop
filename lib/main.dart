ca0af1ac98bc7831935d062f90c85bba3cc6e70d
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-02 08:14:42 -0300

    Navegando para tela do carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
+import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -47,6 +48,7 @@ class MyApp extends StatelessWidget {
         home: const ProductOverviewPage(),
         routes: {
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
+          AppRoutes.cart: (ctx) => const CartPage(),
         },
       ),
     );
