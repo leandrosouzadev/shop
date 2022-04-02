cf7abac26d41ab05f09676a15c5280559ab998e0
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-02 15:30:37 -0300

    Componente gerenciamento de Produto #01

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
+import 'package:shop/pages/products_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
@@ -56,6 +57,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
           AppRoutes.orders: (ctx) => const OrdersPage(),
+          AppRoutes.products: (ctx) => const ProducstPage(),
         },
       ),
     );
