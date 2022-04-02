e1036c2ad8ea46f5888a5aef11be6c354055b71d
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-02 08:55:57 -0300

    Itens do carrinho

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -11,6 +11,29 @@ class CartItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(cartItem.name);
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onBackground,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: FittedBox(
+                child: Text('${cartItem.price}'),
+              ),
+            ),
+          ),
+          title: Text(cartItem.name),
+          subtitle: Text('Total: r\$ ${cartItem.price * cartItem.quantity}'),
+          trailing: Text('${cartItem.quantity}x'),
+        ),
+      ),
+    );
   }
 }
