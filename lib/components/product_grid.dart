169631f848825323d8780c912b4d4b1b03c90d18
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-02 14:29:36 -0300

    Usando Snackbar

--- lib/components/product_grid.dart
+++ lib/components/product_grid.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
+import 'package:shop/components/product_grid_item.dart';
 import 'package:shop/models/product.dart';
 import 'package:shop/models/product_list.dart';
 
@@ -23,7 +23,7 @@ class ProductGrid extends StatelessWidget {
         return ChangeNotifierProvider.value(
           key: GlobalObjectKey(loadedProduct),
           value: loadedProduct,
-          child: const ProductItem(),
+          child: const ProductGridItem(),
         );
       },
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
