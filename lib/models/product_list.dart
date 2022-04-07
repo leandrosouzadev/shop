b71b5eaf16ab842e1d997575678b83b12e420618
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-07 06:55:02 -0300

    Async / Await

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -36,8 +36,8 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> addProduct(Product product) {
-    final future = http.post(
+  Future<void> addProduct(Product product) async {
+    final response = await http.post(
       Uri.parse('$_baseUrl/products.json'),
       body: jsonEncode({
         "name": product.name,
@@ -48,18 +48,16 @@ class ProductList with ChangeNotifier {
       }),
     );
 
-    return future.then<void>((response) {
-      final id = jsonDecode(response.body)['name'];
-      _items.add(Product(
-        id: id,
-        name: product.name,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        isFavorite: product.isFavorite,
-      ));
-      notifyListeners();
-    });
+    final id = jsonDecode(response.body)['name'];
+    _items.add(Product(
+      id: id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      isFavorite: product.isFavorite,
+    ));
+    notifyListeners();
   }
 
   Future<void> updateProduct(Product product) {
