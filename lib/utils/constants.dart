4f5d1270d9c7d4d9c805ab7ed7e593f8a2ad954b
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-09 08:19:31 -0300

    Armazenando os pedidos no firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,7 @@
 class Constants {
   static const productBaseUrl =
       'https://shop-cod3r-92d11-default-rtdb.firebaseio.com/products';
+
+  static const orderBaseUrl =
+      'https://shop-cod3r-92d11-default-rtdb.firebaseio.com/orders';
 }
