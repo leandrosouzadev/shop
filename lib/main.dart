a9614acc7721a821eb4e21d8250d35961e060e29
Leandrosouzaprata <leandro.souzaprata@gmail.com>
2022-04-12 05:37:11 -0300

    Associar pedido ao usuário

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
           create: (_) => OrderList(),
           update: (ctx, auth, previous) => OrderList(
             auth.token ?? '',
+            auth.userId ?? '',
             previous?.items ?? [],
           ),
         ),
